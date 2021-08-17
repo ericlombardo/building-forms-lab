2237ad41c967eb8d1898f67ac3beb502e37c8144
Eric <eric.m.lombardo@gmail.com>
2021-08-17 13:39:37 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,33 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  
+  state = {
+    name: ''
+  }
+
+  handleChange = (e) => { // updates state on keystroke change
+    this.setState({
+      name: e.target.value
+    })
+  }
+  
+  handleSubmit = (e) => {
+    // call dispatch func. from container via props
+    // dispatch needs
+    this.props.addBand(this.state.name)
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            onChange={this.handleChange}
+            type="text"
+          />
+          <input type="submit" />
+        </form>
       </div>
     )
   }
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput />
       </div>
     )
   }
 }
 
-export default BandsContainer
+// create a prop called addBand that takes in a name concats to store
+const mapDispatch = dispatch => {
+  return {
+    addBand: () => dispatch({ type: 'ADD_BAND' })
+  }
+}
+
+export default connect(null, mapDispatch)(BandsContainer)
