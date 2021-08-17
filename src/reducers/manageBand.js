2e3c1b8aaa684f2aa2d3b3e99e08e26e7e14b2fd
Eric <eric.m.lombardo@gmail.com>
2021-08-17 16:25:15 -0400

    Build form to use mapDispatch and mapStateToProps

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,14 @@
+
 export default function manageBand(state = {
   bands: [],
 }, action) {
   switch(action.type) {
     case 'ADD_BAND':
-      return { bands: state.bands.concat(action.payload)}
+      
+    return { 
+      ...state,
+        bands: [...state.bands, action.payload]
+    }
     default:
       return state
   }
